d36e6975981c2a545a2af9b332b8631efed52200
chuonglaptrinh <phamnguyenvanchuong@gmail.com>
2024-10-31 19:05:21 +0700

    lần commit đầu tiên

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+#define ll long long
+#define itmc "......."
+typedef long long ll; = using namespace 
+const float pi = 3.14;
+cout <<"....... \n"; \n xuống dòng
+#include <math.h> // toán 
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello world";
+    return 0;
+}
